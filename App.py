f8a5a83be52f9e3493bd9cd77b48bf404251164b
Aaron Chee <u2102810@siswa.um.edu.my>
2024-10-28 16:38:26 +0800

    Final commit

--- App.py
+++ App.py
@@ -1,13 +1,37 @@
 import streamlit as st
 
-st.title("Embedded Excel Workbook")
+# Set the page configuration before any other Streamlit commands
+st.set_page_config(layout='wide')
 
-# Correctly formatted iframe embed code
-excel_embed_code = """
-<iframe width="2500" height="900" frameborder="0" scrolling="no"
-    src="https://onedrive.live.com/embed?resid=A812EEDD0434CF62%2123166&authkey=%21AK3tjEKWfwoHG6E&em=2&AllowTyping=True&ActiveCell='Dashboard'!E2&Item='Dashboard'!E2%3AAL66&wdHideGridlines=True&wdInConfigurator=True&wdInConfigurator=True">
-</iframe>
+#set background
+page_bg_img = """ 
+<style> 
+.title {
+    color: blue;
+}
+[data-testid="stAppViewContainer"] {
+background-color: #fefefb;
+opacity: 0.9;
+background-image: radial-gradient(#444cf7 0.5px, #fefefb 0.5px);
+background-size: 10px 10px;
+}
+</style> 
 """
 
-# Use st.components.v1.html to display the iframe
-st.components.v1.html(excel_embed_code, height=900, width=2500)
+# Set the background of the page
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+original_title = '<p style=" color: Blue; font-size: 50px; font-weight: Bold; font-size: 50px; font-size: 50px; font-size: 50px;">Household Income Trends and Comparisons in Malaysia (1970-2022)</p>'
+st.markdown(original_title, unsafe_allow_html=True)
+
+# Center the iframe using a div with CSS styling for centering
+excel_embed_code = """
+<div style="display: flex; justify-content: center;">
+    <iframe width="2500" height="1300" frameborder="0" scrolling="no"
+        src="https://onedrive.live.com/embed?resid=A812EEDD0434CF62%2123166&authkey=%21AK3tjEKWfwoHG6E&em=2&AllowTyping=True&ActiveCell='Dashboard'!E2&Item='Dashboard'!E2%3AAL66&wdHideGridlines=True&wdInConfigurator=True&z=15">
+    </iframe>
+</div>
+"""
+
+# Use st.components.v1.html to display the centered iframe
+st.components.v1.html(excel_embed_code, height=1300, width=2500)
